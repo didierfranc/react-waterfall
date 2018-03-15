b2f9bc626e2d4236b643f2d7f4185a145df82218
emilioicai <emiliorodriguez@gmail.com>
2018-03-15 10:21:12 +0100

    added todos example

---
+++ examples/todos/src/components/TodoList.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Todo from './Todo'
+
+const TodoList = ({ todos, toggleTodo, visibilityFilter }) => (
+  <ul>
+    {todos.filter(todo => {
+      switch (visibilityFilter) {
+        case 'SHOW_ALL':
+          return true
+        case 'SHOW_COMPLETED':
+          return todo.completed
+        case 'SHOW_ACTIVE':
+          return !todo.completed
+        default:
+          throw new Error('Unknown filter: ' + visibilityFilter)
+      }
+    }).map(todo =>
+      <Todo
+        key={todo.id}
+        {...todo}
+        onClick={() => toggleTodo(todo)}
+      />
+    )}
+  </ul>
+)
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    completed: PropTypes.bool.isRequired,
+    text: PropTypes.string.isRequired
+  }).isRequired).isRequired,
+  toggleTodo: PropTypes.func.isRequired
+}
+
+export default TodoList

49f3553e1cac3a25c704f5e3ac48514ff5c219a8
Tijs Hendriks <tijs.hendriks@keeyns.com>
2018-10-18 16:11:11 +0200

    fix todos example

--- examples/todos/src/store/actions.js
+++ examples/todos/src/store/actions.js
@@ -1,7 +1,7 @@
 import { v4 as uuid } from 'uuid'
 
 export default {
-  addTodo: ({ todos }, todo) => ({
+  addTodo: ({ todos }, _a, todo) => ({
     todos: [
       ...todos,
       {
@@ -11,11 +11,11 @@ export default {
       },
     ],
   }),
-  toggle: ({ todos }, selected) => ({
+  toggle: ({ todos }, _a, selected) => ({
     todos: todos.map(todo =>
       (todo.id === selected ? { ...todo, completed: !todo.completed } : todo)),
   }),
-  setVisibilityFilter: (_, visibilityFilter) => ({
+  setVisibilityFilter: (_s, _a, visibilityFilter) => ({
     visibilityFilter,
   }),
 }
